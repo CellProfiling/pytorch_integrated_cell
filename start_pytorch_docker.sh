d4da45f98a4df4ba8c9b5debe9bff4d8908a6eea
trangle1302 <lengochaitrang@gmail.com>
2018-02-19 10:46:28 +0100

    test commit.

--- start_pytorch_docker.sh
+++ start_pytorch_docker.sh
@@ -1,6 +1,6 @@
 nvidia-docker run -it \
-	-v /allen/aics/modeling/gregj/projects:/root/projects \
-        -v /allen/aics/modeling/gregj/results:/root/results \
-        -v /raid/gregj/cache:/root/data \
-	gregj/pytorch_extras:dgx_jupyter \
+	-v /home/trang/3D-cell-model/pytorch_integrated_cell:/root/pytorch_integrated_cell \
+        -v /home/trang/3D-cell-model/results:/root/results \
+        -v /home/trang/2D-cell-model/Nuclei_nucleoli/h5:/root/data/h5 \
+        rorydm/pytorch_extras:jupyter \
 	bash 
