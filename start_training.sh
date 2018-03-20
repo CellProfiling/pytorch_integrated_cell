a78def90f0c2044b4d4df47129c0a7f6cd7c567f
trangle1302 <lengochaitrang@gmail.com>
2018-03-20 13:23:09 +0100

    change data paths

--- start_training.sh
+++ start_training.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-cd ..
-python /root/pytorch_integrated_cell/train_model.py \
+# cd /cfs/klemming/nobackup/t/trangle/pytorch_integrated_cell
+
+python pytorch_integrated_cell/train_model.py \
 	--lrEnc 2E-4 --lrDec 2E-4 --lrEncD 1E-2 --lrDecD 2E-4 \
 	--encDRatio 1E-4 --decDRatio 1E-5 \
 	--model_name aaegan3Dv6-exp \
-	--save_dir ./results/nucleoli_test/ \
+	--save_dir ./nucleoli_test/ \
 	--train_module aaegan_trainv6 \
 	--noise 1E-2 \
-	--imdir /root/data/h5 \
+	--imdir /cfs/klemming/nobackup/t/trangle/nucleoli_v18_U2OS_noccd/h5_nuclei \
 	--dataProvider DataProvider2Dfake3D \
 	--saveStateIter 1 --saveProgressIter 1 \
 	--channels_pt1 0 2 --channels_pt2 0 1 2 \
-	--gpu_ids 3 2 \
+	--gpu_ids 0 3 \
 	--batch_size 20  \
 	--nlatentdim 32 \
 	--nepochs 50 \
