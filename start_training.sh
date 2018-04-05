bcf787f13178a2e89eae3cd8dfb547215d34f191
trangle1302 <lengochaitrang@gmail.com>
2018-04-05 10:49:58 +0200

    trang's personal training parameters

--- start_training.sh
+++ start_training.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # cd /cfs/klemming/nobackup/t/trangle/pytorch_integrated_cell
-
-python pytorch_integrated_cell/train_model.py \
-	--lrEnc 2E-4 --lrDec 2E-4 --lrEncD 1E-2 --lrDecD 2E-4 \
+cd ..
+python /root/projects/train_model.py \
+	--lrEnc 2E-4 --lrDec 2E-4 --lrEncD 2E-2 --lrDecD 2E-4 \
 	--encDRatio 1E-4 --decDRatio 1E-5 \
-	--model_name aaegan3Dv6-exp \
-	--save_dir ./nucleoli_test/ \
+	--model_name aaegan_256v3 \
+	--save_dir ./results/nucleoli_1/ \
 	--train_module aaegan_trainv6 \
 	--noise 1E-2 \
-	--imdir /cfs/klemming/nobackup/t/trangle/nucleoli_v18_U2OS_noccd/h5_nuclei \
-	--dataProvider DataProvider2Dfake3D \
+	--imdir /root/data/PNG \
+	--dataProvider DataProvider2DPNG \
 	--saveStateIter 1 --saveProgressIter 1 \
 	--channels_pt1 0 2 --channels_pt2 0 1 2 \
-	--gpu_ids 0 3 \
-	--batch_size 20  \
-	--nlatentdim 32 \
-	--nepochs 50 \
-	--nepochs_pt2 50
+	--gpu_ids 0 1 \
+	--batch_size 50  \
+	--nlatentdim 16 \
+	--nepochs 150 \
+	--nepochs_pt2 150
