d1fe2503ef7fc76656aa026a04916a58ac3dce7d
trangle1302 <lengochaitrang@gmail.com>
2018-04-05 10:48:53 +0200

    trang@codex binding paths

--- start_docker.sh
+++ start_docker.sh
@@ -1,6 +1,6 @@
 nvidia-docker run -it \
-	-v /allen/aics/modeling/gregj/projects:/root/projects \
-        -v /allen/aics/modeling/gregj/results:/root/results \
-        -v /raid/gregj/cache:/root/data \
-	gregj/pytorch_extras:dgx_jupyter \
+	-v /home/trang/Desktop/3Dcell/pytorch_integrated_cell:/root/projects \
+        -v /home/trang/Desktop/results:/root/results \
+        -v /home/trang/Desktop/nucleoli_v18_U2OS_noccd:/root/data \
+	rorydm/pytorch_extras:jupyter \
 	bash 
